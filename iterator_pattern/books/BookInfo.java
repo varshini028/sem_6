b9b9ff0b363e8da1ef044eed8ed9a41a58c2dec6
varshini028 <58366142+varshini028@users.noreply.github.com>
2020-04-26 01:29:31 +0530

    Update BookInfo.java

--- iterator_pattern/books/BookInfo.java
+++ iterator_pattern/books/BookInfo.java
@@ -1,1 +1,19 @@
 
+package books;
+
+public class BookInfo {
+
+	String BookName; 
+	String AuthorName; 
+	int yearReleased;
+	public  BookInfo(String newBookName,String newAuthorName, int newyearReleased)
+	{	BookName=newBookName;
+		AuthorName=newAuthorName;
+		yearReleased=newyearReleased;
+	}
+	public String getBookName(){ return BookName; }
+	public String getAuthorName(){ return AuthorName; }
+	public int getYearReleased(){ return yearReleased; }
+	
+	
+}
