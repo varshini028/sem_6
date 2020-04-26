21c7b44fb103310664ca812565c85c577988c237
varshini028 <58366142+varshini028@users.noreply.github.com>
2020-04-26 13:19:30 +0530

    Update Srishti_Publishers.java

--- iterator_pattern/books/Srishti_Publishers.java
+++ iterator_pattern/books/Srishti_Publishers.java
@@ -1,1 +1,38 @@
 
+package books;
+
+import java.util.Hashtable;
+
+import java.util.Iterator;
+
+public class Srishti_Publishers implements BookIterator{
+
+    Hashtable<Integer, BookInfo> books = new Hashtable<Integer, BookInfo>();
+    int hashKey = 0;
+
+    public Srishti_Publishers() {
+
+        addBook("The Imperfect", "Prasoon", 2017);
+        addBook("The One From The Stars", "Keshav Anand", 2017);
+        addBook("A Silent Promise", "Namrata Gupta", 2015);
+
+    }
+
+    public void addBook(String BookName, String AuthorName, int yearReleased){
+
+        BookInfo bookInfo = new BookInfo(BookName, AuthorName, yearReleased);
+
+        bestSongs.put(hashKey, bookInfo);
+
+        hashKey++;
+
+    }
+
+    public Iterator createIterator() {
+        return books.values().iterator();
+
+    }
+
+}
+
+
