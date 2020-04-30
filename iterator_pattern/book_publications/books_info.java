e0226f9487f9922a7ba93ef89ef070fcba3324a4
varshini028 <58366142+varshini028@users.noreply.github.com>
2020-04-30 11:58:13 +0530

    Update books_info.java

--- iterator_pattern/book_publications/books_info.java
+++ iterator_pattern/book_publications/books_info.java
@@ -1,28 +1,33 @@
 package books_publlications;
 
-public class books_info {
+public class books_info
+{
 
 
 	final String name;
 	final String author;
 	final int year;	
 	
-	public books_info( final String name,final String author,final int year){
+	public books_info( final String name,final String author,final int year)
+	{
 		this.name = name;
 		this.author = author;
 		this.year = year;
 
 	}
 	
-	public String getName() {
+	public String getName() 
+	{
 		return name;
 	}
 
-	public String getauthor() {
+	public String getauthor() 
+	{
 		return author;
 	}
 
-	public int getyear() {
+	public int getyear() 
+	{
 		return year;
 	}
 	
